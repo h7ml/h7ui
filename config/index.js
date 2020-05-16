6be34c507bcabdba89cc2b4fba938f24392b8fe0
h7ml <h7ml@qq.com>
2020-05-16 16:21:55 +0800

    deploy

--- config/index.js
+++ config/index.js
@@ -45,12 +45,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../gh-page/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '..//gh-page'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/gh-page',
 
     /**
      * Source Maps
